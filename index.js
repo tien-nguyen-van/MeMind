cfda21de82468c53edcf8b0c84414b3ab6861209
tien-nguyen-van <tien.nv.pm@gmail.com>
2021-09-16 22:41:15 +0700

    feat: add firebase to project

--- index.js
+++ index.js
@@ -3,7 +3,23 @@
  */
 
 import { AppRegistry } from 'react-native'
+import React from 'react'
 import App from './App'
 import { name as appName } from './app.json'
+import messaging from '@react-native-firebase/messaging'
 
-AppRegistry.registerComponent(appName, () => App)
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage)
+})
+
+const headlessCheck = ({ isHeadless }) => {
+  if (isHeadless) {
+    // App has been launched in the background by iOS, don't render
+    return null
+  }
+
+  return <App />
+}
+
+AppRegistry.registerComponent(appName, () => headlessCheck)
